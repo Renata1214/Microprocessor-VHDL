d4dd60f431c0aa5a1e95ff5c4e29d6955e5c629b
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2024-06-20 04:27:59 +0000

    Changes

---
+++ Controller.vhd
@@ -0,0 +1,101 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+use work.common.all;
+
+entity Controller is
+	port ( 	opcode : in opcode_type; --operation to be carried out by alu
+
+	 	operand_1 : out STD_LOGIC_VECTOR (13 downto 0); --14bit to output to register
+	 	operand_2 : out STD_LOGIC_VECTOR (13 downto 0); --14bit to output to register
+
+	 	result : in STD_LOGIC_VECTOR (13 downto 0); --returned from register file
+
+		curr_PC : in STD_LOGIC_VECTOR (6 downto 0); --incremented or sent to branch based on opcode
+
+		new_PC : out STD_LOGIC_VECTOR (6 downto 0);
+		PC_we : out STD_LOGIC; --enable signal for PC (true or false)
+		PC_incr : out STD_LOGIC; --if not invlolving branch
+
+		Rs1_data : in STD_LOGIC_VECTOR (13 downto 0); --14bit input operand rs1
+		Rs2_data : in STD_LOGIC_VECTOR (13 downto 0); --14 bit input rs2
+		immediate : in STD_LOGIC_VECTOR (13 downto 0); -- immediate value if any recieved from decoder
+
+		Rd_we : out STD_LOGIC; --enable signal for Rd (true or false)
+		Rd_data : out STD_LOGIC_VECTOR (13 downto 0) --output data to be stored in rd location
+	     );
+end Controller;
+
+architecture Behavioral of Controller is
+
+begin
+
+control : process (opcode, Rs1_data, Rs2_data, result, immediate, curr_PC)
+begin
+	-- default assignments, can be overwritten below
+	operand_1 <= Rs1_data;
+	operand_2 <= Rs2_data;
+
+	Rd_we <= '0';
+	Rd_data <= result;
+
+	PC_we <= '0';
+	new_PC <= (6 downto 0 => 'X');
+	PC_incr <= '0';
+
+	-- regular operations with Rs1, Rs2, Rd
+	-- TODO consider remaining cases
+	if ((opcode = OP_ADD) or (opcode = OP_OR)  or (opcode = OP_XOR) or (opcode = OP_AND)  or (opcode = OP_SUB)) then
+
+		Rd_we <= '1';
+		PC_incr <= '1';
+
+	-- TODO implement remaining cases
+	elsif ((opcode = OP_ADDI) or (opcode = OP_ORI)  or (opcode = OP_XORI) or (opcode = OP_SLL)  or (opcode = OP_SRL)or (opcode = OP_ANDI)or (opcode = OP_SUBI) ) then 
+		operand_2 <= immediate;
+		Rd_we <= '1';
+		PC_incr <= '1';
+		
+	elsif (opcode = OP_BE) then
+		Rd_we <= '0';
+		if (Rs1_data = Rs2_data) then 
+			operand_1 <= ("0000000" & curr_PC);
+			operand_2 <= immediate;
+			new_PC <= result(6 downto 0);
+			PC_we <= '1';
+		else
+			PC_incr <= '1';
+			PC_we <= '0';
+		end if;
+		
+	elsif (opcode = OP_BLT) then
+		Rd_we <= '0';
+		if (Rs1_data < Rs2_data) then 
+			operand_1 <= ("0000000" & curr_PC);
+			operand_2 <= immediate;
+			new_PC <= std_logic_vector(result(6 downto 0));
+			PC_we <= '1';
+		else
+			PC_we <= '0';
+			PC_incr <= '1';
+		end if;
+	
+	elsif (opcode = OP_JMP) then
+		Rd_we <= '0';
+		PC_we <= '1';
+		operand_1 <= ("0000000" & curr_PC);
+		operand_2 <= immediate;
+		new_PC <= std_logic_vector(result(6 downto 0));
+
+	-- only OP_HALT should remain
+	else
+		operand_1 <= (13 downto 0 => 'X');
+		operand_2 <= (13 downto 0 => 'X');
+
+		Rd_data <= (13 downto 0 => 'X');
+	end if;
+end process;
+
+end Behavioral;
+
---
+++ Decoder.vhd
@@ -0,0 +1,109 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+use work.common.all;
+
+entity Decoder is
+	port ( 	instruction_in : in STD_LOGIC_VECTOR (15 downto 0);
+
+		opcode_out : out opcode_type;
+
+		Rd_addr_out : out STD_LOGIC_VECTOR (2 downto 0);
+		Rs1_addr_out : out STD_LOGIC_VECTOR (2 downto 0);
+		Rs2_addr_out : out STD_LOGIC_VECTOR (2 downto 0);
+
+		immediate_out : out STD_LOGIC_VECTOR (13 downto 0)
+	     );
+end Decoder;
+
+architecture Behavioral of Decoder is
+
+--TODO add signals as needed
+signal opcode_internal : opcode_type;
+signal 	Rd_addr_sig : STD_LOGIC_VECTOR (2 downto 0);
+signal	Rs1_addr_sig : STD_LOGIC_VECTOR (2 downto 0);
+signal	Rs2_addr_sig :  STD_LOGIC_VECTOR (2 downto 0);
+signal	immediate_sig : STD_LOGIC_VECTOR (13 downto 0);
+
+
+begin
+
+
+	opcode_out <= opcode_internal;
+
+	opcode_internal <= std_logic_vector_to_opcode_type( instruction_in(15 downto 12) );
+
+	--TODO implement extraction of remaining parts of the instruction
+	Rd_addr_sig <= std_logic_vector(instruction_in(11 downto 9));
+	Rd_addr_out <= Rd_addr_sig;
+	
+	Rs1_addr_sig <= instruction_in(8 downto 6);
+	Rs1_addr_out <= Rs1_addr_sig;
+		
+	Rs2_addr_sig <= instruction_in(5 downto 3);
+	Rs2_addr_out <= Rs2_addr_sig;
+	
+	immediate_out <= immediate_sig;
+
+	--TODO derive immediate value, depending on opcode_internal
+decode : process (opcode_internal)
+begin
+	case opcode_internal is 
+		when (OP_ANDI) => 
+			immediate_sig <= ("11111111" & Rs2_addr_sig & instruction_in (2 downto 0));
+		when (OP_ORI) =>
+			immediate_sig <=  ("00000000" & Rs2_addr_sig & instruction_in (2 downto 0));
+		when(OP_XORI)=>
+			immediate_sig <=  ("00000000" & Rs2_addr_sig & instruction_in (2 downto 0));
+		when (OP_SLL ) =>
+			immediate_sig <= ("00000000000" & instruction_in (2 downto 0));
+		when (OP_SRL) =>
+			immediate_sig <= ("00000000000" & instruction_in (2 downto 0));
+			
+		when (OP_ADDI) =>
+			if instruction_in (5) = '1' then 
+			immediate_sig <= ("11111111" & Rs2_addr_sig & instruction_in (2 downto 0)); 
+			else 
+			immediate_sig <= ("00000000" & Rs2_addr_sig & instruction_in (2 downto 0)); 
+			end if;
+			--CHECK
+		when (OP_SUBI) =>
+			if instruction_in (5) = '1' then
+			immediate_sig <= ("11111111" & Rs2_addr_sig & instruction_in (2 downto 0));
+			else
+			immediate_sig <= ("00000000" & Rs2_addr_sig & instruction_in (2 downto 0)); --CHECK
+			end if;
+		when (OP_BLT) =>
+		
+			if instruction_in (5) = '1' then 
+			immediate_sig <= ("11111111" & Rd_addr_sig & instruction_in (2 downto 0));
+			else
+			immediate_sig <= ("00000000" & Rd_addr_sig & instruction_in (2 downto 0)); --CHECK
+			end if;
+			
+		when (OP_BE) =>
+			if instruction_in (5) = '1' then 
+			immediate_sig <= ("11111111" & Rd_addr_sig & instruction_in (2 downto 0));
+			else
+			immediate_sig <= ("00000000" & Rd_addr_sig & instruction_in (2 downto 0)); --CHECK
+			end if;
+			
+		when (OP_JMP) =>
+			if instruction_in (5) = '1' then 
+			immediate_sig <= ("11111111" & Rd_addr_sig & instruction_in (2 downto 0));
+			else
+			immediate_sig <= ("00000000" & Rd_addr_sig & instruction_in (2 downto 0)); --CHECK
+			end if;
+			
+		when others => 
+			immediate_sig <= "00000000000000";
+			
+	end case;
+	
+
+	
+	end process;
+
+
+end Behavioral;
